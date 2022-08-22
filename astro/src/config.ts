7770a024d4dba04c8cd5071622eca8819fde070d
Andreas Lennartz <mail@etlbox.net>
2022-08-22 20:42:29 +0200

    Basic configuration and assets

--- astro/src/config.ts
+++ astro/src/config.ts
@@ -1,17 +1,16 @@
 export const SITE = {
-	title: 'Documentation',
-	description: 'Your website description.',
+	title: 'ETLBox',
+	description: 'A lightweight ETL (extract, transform, load) toolbox and data integration library for .NET',
 	defaultLanguage: 'en_US',
 };
 
 export const OPEN_GRAPH = {
 	image: {
-		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true',
+		src: 'https://www.etlbox.net/android-chrome-512x512.png',
 		alt:
-			'astro logo on a starry expanse of space,' +
-			' with a purple saturn-like planet floating in the right foreground',
+			'ETLBox'
 	},
-	twitter: 'astrodotbuild',
+	twitter: 'etlbox',
 };
 
 export const KNOWN_LANGUAGES = {
@@ -19,18 +18,18 @@ export const KNOWN_LANGUAGES = {
 };
 
 // Uncomment this to add an "Edit this page" button to every page of documentation.
-// export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/blob/main/docs/`;
+export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/blob/main/docs/`;
 
 // Uncomment this to add an "Join our Community" button to every page of documentation.
 // export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
 // Uncomment this to enable site search.
 // See "Algolia" section of the README for more information.
-// export const ALGOLIA = {
-//   indexName: 'XXXXXXXXXX',
-//   appId: 'XXXXXXXXXX',
-//   apiKey: 'XXXXXXXXXX',
-// }
+export const ALGOLIA = {
+    indexName: 'XXXXXXXXXX',
+    appId: 'XXXXXXXXXX',
+    apiKey: 'XXXXXXXXXX',
+}
 
 export const SIDEBAR = {
 	en: [
